4779d67657962f6f6121f045635fdb34a492c961
jay-xzj <xzj_thrive@126.com>
2023-02-17 23:51:27 +0800

    configure jest test suite

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('strict-ng-hero app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('strict-ng-hero');
   });
 });
